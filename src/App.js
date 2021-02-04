78b96a2d8b2fb518ae76e5100e602660a700b194
djbarcelos <djbarcelos@gmail.com>
2021-02-04 20:23:08 -0300

    Att

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react'
-import './App.css';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
+import { Home } from './Home';
+import { About } from './About';
+import { Contact } from './Contact';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
+import { Jumbotron } from './components/Jumbotron';
+
 class App extends Component {
-  render(){
+  render() {
     return (
       <React.Fragment>
         <Router>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/About' component={About} />
-          </Switch>
+          <NavigationBar />
+          <Jumbotron />
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
         </Router>
       </React.Fragment>
     );
